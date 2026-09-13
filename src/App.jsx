d79db0407b220af99a30d5c34c553f50d7d3be22
Kiran R <kiran.r1497@gmail.com>
2026-09-13 11:02:23 +0530

    second commit - MealsSummary message

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Header from "./components/Layout/Header";
+import MealsSummary from "./components/Meals/MealsSummary";
+import "./App.css";
 
 function App() {
   return (
     <div>
       <Header />
 
-      <h2>Let's get started</h2>
+      <main>
+        <div className="hero-image"></div>
+
+        <MealsSummary />
+      </main>
     </div>
   );
 }
